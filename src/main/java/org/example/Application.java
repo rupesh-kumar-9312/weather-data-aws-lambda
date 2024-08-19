b4cb9314ac7fba614ee74ff7f682b41a0e3b35a5
rupeshkumar <rupeshkumar9312@gmail.com>
2024-08-19 13:32:36 +0530

    update pom.xml

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -11,7 +11,7 @@ import org.example.controller.PingController;
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "org.example.controller")
-@Import({ WeatherController.class })
+@Import({ PingController.class })
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/org/example/controller/PingController.java
+++ src/main/java/org/example/controller/PingController.java
@@ -1,8 +1,12 @@
 package org.example.controller;
 
 
+import org.example.service.WeatherFunction;
+import org.example.service.WeatherService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import reactor.core.publisher.Mono;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -10,10 +14,22 @@ import java.util.Map;
 
 @RestController
 public class PingController {
+    @Autowired
+    private WeatherService weatherService;
+
+    @Autowired
+    private WeatherFunction weatherFunction;
+
+    @RequestMapping(path = "/ping/{city}", method = RequestMethod.GET)
+    public Mono<String> getWeatherData(@PathVariable String city){
+        return weatherFunction.getWeather().apply(city);
+    }
+
     @RequestMapping(path = "/ping", method = RequestMethod.GET)
     public Map<String, String> ping() {
         Map<String, String> pong = new HashMap<>();
         pong.put("pong", "Hello, World!");
         return pong;
     }
+
 }
